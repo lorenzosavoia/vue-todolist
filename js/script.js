3d0170d43715d3e0a266570938bbd24bd4b64dab
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-17 12:43:56 +0100

    problema

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        
+        todos: [
+            {
+                tag: 'introducing',
+                text: 'lorem ipsum',
+                author: 'lorenzo'
+            },
+            {
+                tag: 'introducing',
+                text: 'lorem ipsum',
+                author: 'mattia'
+            },
+            {
+                tag: 'introducing',
+                text: 'lorem ipsum',
+                author: 'luca'
+            },
+        ]
+    },
+    methods: {
+
+    }
+})
